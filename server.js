1736a4c624e19691cdaa9712e94d54de0afa0303
pifernandez <pilfernandez1@gmail.com>
2019-09-29 19:43:50 -0300

    wip

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 const express = require('express')
 const logger = require('morgan')
-<<<<<<< HEAD
-const cors = require('cors');
-const bodyParser = require('body-parser');
-=======
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
->>>>>>> fc6e695ec7c8794947e004622850463d399ed11c
 const router = require('./modules/router')
 
 const server = express()
@@ -19,19 +14,10 @@ server.use(
 	})
 );
 server.use(bodyParser.json());
-<<<<<<< HEAD
-
-server.use(router)
-server.use('/assets', express.static('public'));
-server.use(logger('dev'))
-server.use(cors);
-
-=======
 server.use(logger('dev'));
 server.use('/assets', express.static('public'));
 server.use(router);
 server.use(cors);
->>>>>>> fc6e695ec7c8794947e004622850463d399ed11c
 
 server.listen(port, () => {
     console.log(`running on port ${port}`)
